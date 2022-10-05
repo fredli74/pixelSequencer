35f367948e7e8e2ff2fb087551552521d93a0c3d
Fredrik Lidström <fredli74@gmail.com>
2022-10-05 15:27:09 +0200

    No openmp version to avoid dependencies

---
+++ buildlib_openmp.bat
@@ -0,0 +1,6 @@
+del libimagequant_win.a
+cd libimagequant
+gcc -w -c -O3 -fno-math-errno -fopenmp -funroll-loops -fomit-frame-pointer -Wall -Wno-attributes -std=c99 -DNDEBUG -DUSE_SSE=1 -msse -fexcess-precision=fast^
+  pam.c mediancut.c blur.c mempool.c kmeans.c nearest.c libimagequant.c
+ar rcs ../libimagequant_win.a *.o
+cd ..
